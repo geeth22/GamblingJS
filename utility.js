9c3f326730e355cf3cf64625919e317877267961
geeth22 <geethchandra22@gmail.com>
2020-11-25 23:54:55 +0530

    refactor: check whether won or lost

--- utility.js
+++ utility.js
@@ -1,2 +1,27 @@
-let betamount = 1;
-let stake = 1;
+class GamblerSimulation{
+    Stake = 100
+    betamount = 1
+   
+    constructor(win, lose){
+       this.win = win;
+       this.lose = lose;
+   }
+   checkWinOrLoose(){
+       return Math.floor(Math.random() * Math.floor(2));
+   }
+   GamblerWinOrLoose(){
+
+       let result = this.checkWinOrLoose();
+       if (result == 1) {
+           this.win += 1;
+           this.Stake += 1;
+           console.log( "You won 1 dollar")
+       }
+       if (result == 0) {
+           this.lose += 1;
+           this.Stake -= 1;
+           console.log("You lost 1 dollar")
+       }
+    }
+   }
+   module.exports = new GamblerSimulation();
